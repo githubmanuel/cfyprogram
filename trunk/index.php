f2ac97ae4d0f68e9aae4545fbf1bd16ca2719ea6
mail@pajarraco.com <mail@pajarraco.com@9b889539-2bc9-7747-62f0-05d52d81161f>
2010-09-01 21:08:06 +0000

    testing svn on google code

---
+++ trunk/index.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>CFY Program</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  </head>
+  <body>
+    CFY Business Management Suite
+  </body>
+</html>

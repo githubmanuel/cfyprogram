a70e72f4450025a486fd1da14d62c1c1e1404208
mail@pajarraco.com <mail@pajarraco.com@9b889539-2bc9-7747-62f0-05d52d81161f>
2010-09-10 20:10:33 +0000

    license

--- trunk/index.php
+++ trunk/index.php
@@ -1,9 +1,18 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
 
+CFY program - CFY Business Management Suite
+
+Integrated enterprise applications to execute and optimize business and IT strategies. Enable you to perform essential, industry-specific, and business-support processes with modular solutions.
+
+Version: 0.0.0.1a
+Author: Ernesto La Fontaine
+License: New BSD License (see docs/license.txt)
+
+File: index.php
+Commnents: Startup file.
+
+*/
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
